f060d2b0ba46a31206995f136df49583e6e8126e
Ahmed Ibrahim <ib153507@gmail.com>
2021-08-17 16:59:08 +0200

    Setup the database

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,9 +1,14 @@
-import express, { Response } from "express";
+import express from "express";
+import cors from "cors";
+import connectDB from "./database/db";
+import dotenv from "dotenv";
 const app = express();
 const port = 3000;
-app.get("/", (req, res) => {
-  res.send("The sedulous hyena ate the antelope!");
-});
+dotenv.config();
+app.use(express.json());
+app.use(cors());
+connectDB();
+
 app.listen(port, () => {
-  return console.log(`server is listening on ${port}`);
+  return console.log(`server is listening on ${port}`.bgGreen);
 });
